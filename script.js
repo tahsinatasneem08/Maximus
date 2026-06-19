4e0f31c4db7555b65d7b826d9d68ecf035fff274
Tahsina Tasneem <tahsinatasneem08@gmail.com>
2026-06-19 20:39:02 +0600

    working on who section

--- script.js
+++ script.js
@@ -27,3 +27,63 @@ button.addEventListener("click", () => {
 
     })();
 });
+
+
+
+
+const card = document.querySelector(".left-card");
+
+let isFlipped = false;
+
+/* TILT */
+
+card.addEventListener("mousemove", (e) => {
+
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateY = (x - centerX) / 20;
+    const rotateX = -(y - centerY) / 20;
+
+    const flipRotation = isFlipped ? 180 : 0;
+
+    card.style.transform =
+        `rotateY(${flipRotation + rotateY}deg)
+         rotateX(${rotateX}deg)`;
+});
+
+/* RESET */
+
+card.addEventListener("mouseleave", () => {
+
+    const flipRotation = isFlipped ? 180 : 0;
+
+    card.style.transform =
+        `rotateY(${flipRotation}deg)
+         rotateX(0deg)`;
+
+});
+
+/* DOUBLE CLICK FLIP */
+
+card.addEventListener("dblclick", () => {
+
+    isFlipped = !isFlipped;
+
+    card.style.transition = "transform 0.7s ease";
+
+    card.style.transform =
+        isFlipped
+        ? "rotateY(180deg)"
+        : "rotateY(0deg)";
+
+    setTimeout(() => {
+        card.style.transition = "transform 0.15s ease";
+    }, 700);
+
+});
